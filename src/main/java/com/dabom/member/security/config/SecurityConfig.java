d964c7eb55cf1fa0f4b275a5890b138b80711601
tipsyboy <gudah13@naver.com>
2025-09-22 16:23:27 +0900

    link front uri

--- src/main/java/com/dabom/member/security/config/SecurityConfig.java
+++ src/main/java/com/dabom/member/security/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
         configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:5173"
+                frontServer
         ));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
